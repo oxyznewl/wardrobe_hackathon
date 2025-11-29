6e7b931819a2537098633de24026377f2768f952
chaeyeon-sim <tlacodus630@gmail.com>
2025-11-29 10:45:06 +0900

    샘플 데이터 삭제 및 문구(옷이 없습니다) 추가

--- src/components/ClothesList.jsx
+++ src/components/ClothesList.jsx
@@ -38,16 +38,22 @@ const ClothesList = ({ seasons, category, sort, onItemClick }) => {
     });
 
   return (
-    <Grid>
-      {filtered.map((item) => (
-        <ClothesCard
-          id={item.id}
-          item={item.name}
-          image={item.image}
-          onClick={onItemClick ? () => onItemClick(item) : undefined}
-        />
-      ))}
-    </Grid>
+    <>
+      {clothes.length === 0 ? (
+        <h3>옷이 없습니다. 옷장에 옷을 추가해보세요.</h3>
+      ) : (
+        <Grid>
+          {filtered.map((item) => (
+            <ClothesCard
+              id={item.id}
+              item={item.name}
+              image={item.image}
+              onClick={onItemClick ? () => onItemClick(item) : undefined}
+            />
+          ))}
+        </Grid>
+      )}
+    </>
   );
 };
 
